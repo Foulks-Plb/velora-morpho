485456b0e7fe15eb6ad48dd18d420b9fd4d150bd
Foulk plb | Morpho <71005796+Foulks-Plb@users.noreply.github.com>
2025-08-01 15:01:56 +0200

    fix: delete max-multiplier.ts

--- max-multiplier.ts
+++
@@ -1,7 +0,0 @@
-function maxMultiplier(LTV, LLTV, slippage) {
-  if (LTV >= LLTV || 1 + slippage <= LLTV) return 1;
-  return (1 + slippage - LTV) / (1 + slippage - LLTV);
-}
-
-/* Exemple */
-console.log(maxMultiplier(0.0, 0.7, 0.0).toFixed(2));
